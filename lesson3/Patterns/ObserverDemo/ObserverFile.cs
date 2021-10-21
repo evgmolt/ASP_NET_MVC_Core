013d732e2f4ad3f5715ff56516492c3cb73add3e
evgmolt <evg.molt@gmail.com>
2021-10-21 13:28:23 +0400

    Observer demo completed

---
+++ lesson3/Patterns/ObserverDemo/ObserverFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverDemo
+{
+    class ObserverFile : IObserver
+    {
+        private const string _fileName = "keys.txt";
+        public void Update(object ob)
+        {
+            ConsoleKeyInfo k = (ConsoleKeyInfo)ob;
+            File.AppendAllText(_fileName, k.KeyChar.ToString() + " ");
+        }
+    }
+}
