88f3b642a7aa670fd28db77214f7c8c4f4905473
evgmolt <evg.molt@gmail.com>
2021-11-08 12:43:13 +0400

    Homework 5 completed

---
+++ lesson5/Scanner/Program.cs
@@ -0,0 +1,24 @@
+﻿using Emulator;
+using ScannerLib;
+using System;
+using System.Threading;
+
+namespace Scanner
+{
+    class Program
+    {
+        static void Main()
+        {
+            Device scanner = new();
+            Driver driver = new(scanner);
+            driver.SetupSaveType(new SaverAsText());
+            driver.Save("textfile.txt");
+            driver.SetupSaveType(new SaverAsBin());
+            driver.Save("binfile.bin");
+            var load = driver.GetLoad();
+            Console.WriteLine($"RAM : {load.RAM}");
+            Console.WriteLine($"CPU : {load.CPU}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ lesson5/ScannerLib/SaverAsText.cs
@@ -0,0 +1,32 @@
+﻿using Emulator;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScannerLib
+{
+    public sealed class SaverAsText : ISaver
+    {
+        public bool Save(IDevice device, string fileName)
+        {
+            byte[] data = device.GetData();
+            string s = "";
+            for (int i = 0; i < data.Length; i++)
+            {
+                s += data[i].ToString() + " ";
+            }
+            try
+            {
+                File.WriteAllText(fileName, s);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
