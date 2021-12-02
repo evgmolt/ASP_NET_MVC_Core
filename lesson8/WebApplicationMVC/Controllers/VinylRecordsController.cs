9c66f0d24f72c58ee1f9336af4d88feea74574c4
evgmolt <evg.molt@gmail.com>
2021-12-02 15:09:06 +0400

    Controller, model and view added. EF migration Ok

---
+++ lesson8/WebApplicationMVC/Controllers/VinylRecordsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplicationMVC.Data;
+using WebApplicationMVC.Models;
+
+namespace WebApplicationMVC.Controllers
+{
+    public class VinylRecordsController : Controller
+    {
+        private readonly WebApplicationMVCContext _context;
+
+        public VinylRecordsController(WebApplicationMVCContext context)
+        {
+            _context = context;
+        }
+
+        // GET: VinylRecords
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.VinylRecord.ToListAsync());
+        }
+
+        // GET: VinylRecords/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vinylRecord = await _context.VinylRecord
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vinylRecord == null)
+            {
+                return NotFound();
+            }
+
+            return View(vinylRecord);
+        }
+
+        // GET: VinylRecords/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VinylRecords/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Artist,Album,Label,RealeaseDate")] VinylRecord vinylRecord)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vinylRecord);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vinylRecord);
+        }
+
+        // GET: VinylRecords/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vinylRecord = await _context.VinylRecord.FindAsync(id);
+            if (vinylRecord == null)
+            {
+                return NotFound();
+            }
+            return View(vinylRecord);
+        }
+
+        // POST: VinylRecords/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Artist,Album,Label,RealeaseDate")] VinylRecord vinylRecord)
+        {
+            if (id != vinylRecord.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(vinylRecord);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VinylRecordExists(vinylRecord.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vinylRecord);
+        }
+
+        // GET: VinylRecords/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vinylRecord = await _context.VinylRecord
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (vinylRecord == null)
+            {
+                return NotFound();
+            }
+
+            return View(vinylRecord);
+        }
+
+        // POST: VinylRecords/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var vinylRecord = await _context.VinylRecord.FindAsync(id);
+            _context.VinylRecord.Remove(vinylRecord);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VinylRecordExists(int id)
+        {
+            return _context.VinylRecord.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ lesson8/WebApplicationMVC/Migrations/20211202103953_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplicationMVC.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VinylRecord",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Artist = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Album = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Label = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RealeaseDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VinylRecord", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VinylRecord");
+        }
+    }
+}
